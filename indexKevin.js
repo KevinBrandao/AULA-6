ae6d4e4dc5e58fd683bd52f83fd05c149eb0efd6
KevenBrandao <kevenbrandaoms@outlook.com>
2023-09-29 20:18:03 -0300

    calculadora vue

--- indexKevin.js
+++ indexKevin.js
@@ -7,10 +7,22 @@ createApp({
     },
       methods: {
         lidarBotao(value) {
+          // console.log(numero);
+          // if(this.display === '0'){
+          //   this.display = numero;
+          // } else {
+          //   this.display += value;
+          // }
           this.display += value;
         },
         limparDisplay() {
           this.display = '';
+        },
+        lidarDecimal(){
+          console.log(".");
+          if(!this.display.includes(".")){
+            this.display += "."
+          }
         },
         calcularResultado() {
           try {
